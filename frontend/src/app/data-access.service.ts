96ea8d6ad774d39c3500335c8d88ebeb8582490d
mukulkant-mikemighty <mukulkantjohar@gmail.com>
2022-01-06 19:00:30 +0530

    c2
    erfe

--- frontend/src/app/data-access.service.ts
+++ frontend/src/app/data-access.service.ts
@@ -14,12 +14,12 @@ export class DataAccessService {
     ) {}
   executeDataAccessDefaultService(){
     console.log("Hi I am Data Access Default Service");
-    return this.http.get("http://localhost:4402");
+    return this.http.get("https://wikifend.herokuapp.com/");
   }
   
   executeDataAccessBeanService(){
     console.log("Hi I am Data Access Bean Service");
-    return this.http.get("http://localhost:4402/helloworld-bean");
+    return this.http.get("https://wikifend.herokuapp.com/helloworld-bean");
   }
 
   executeDataDeleteService(pageId1:any){
@@ -27,13 +27,13 @@ export class DataAccessService {
     console.log(pageId1);
     //let id=pageId1;
     //return this.http.get(`http://localhost:4402/delete/${pageId1}`);//But to catch this, there should be a request mapping with /delete/ in the path
-    return this.http.delete(`http://localhost:4402/${pageId1}`);
+    return this.http.delete(`https://wikifend.herokuapp.com/${pageId1}`);
   }
 
   executeDataSaveService(page2:any){
     
     console.log(page2);
     //let id=pageId1;
-    return this.http.post('http://localhost:4402',{ 'searchList' : page2 });
+    return this.http.post('https://wikifend.herokuapp.com/',{ 'searchList' : page2 });
   }
 }
